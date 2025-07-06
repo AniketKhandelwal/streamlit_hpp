e9f12d60a9a435bb27acfb053b8000cde4ab1979
AniketKhandelwal <khandelwalaniket070@gmail.com>
2025-07-06 21:04:54 +0530

    Updated path of the saved files

--- project/predict_pipeline.py
+++ project/predict_pipeline.py
@@ -1,25 +1,33 @@
 import pandas as pd
 import joblib
 from preprocessing import categorical_encoding
+import os
 
-# Load the model and scaler
-model = joblib.load("model/linear_model.pkl")
-scaler = joblib.load("scaler/scaler.pkl")
-features_list = joblib.load("model/features_list.pkl")  # List of features used during training
+base_dir = os.path.dirname(__file__)
+
+model_path = os.path.join(base_dir,"model","linear_model.pkl")
+scaler_path = os.path.join(base_dir, "scaler", "scaler.pkl")
+features_list_path = os.path.join(base_dir, "model", "features_list.pkl")
+
+
+model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
+features_list = joblib.load(features_list_path)
+
+ 
 
 def predict_price(input_df):
-    # 1. Apply encoding (ordinal + one-hot)
+   
     encoded_input = categorical_encoding(input_df)
 
-    # 2. Add any missing columns
+  
     for col in features_list:
         if col not in encoded_input.columns:
             encoded_input[col] = 0
 
-    # 3. Ensure correct column order
+    
     encoded_input = encoded_input[features_list]
 
-    # 4. Scale and predict
     scaled_input = scaler.transform(encoded_input)
     prediction = model.predict(scaled_input)
 
